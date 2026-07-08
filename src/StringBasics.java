a2d504a3b6093c72346b3663fbdb5a4548ef72fb
muskanmishra0607 <muskanmishra0607@gmail.com>
2026-07-08 23:25:37 +0530

    String

---
+++ src/StringBasics.java
@@ -0,0 +1,12 @@
+public class StringBasics {
+    static void main() {
+
+        String firstName = "Virat";
+        String lastName = "Kohli";
+        System.out.println("Name: " + firstName + " " + lastName);
+
+// Accessing the Element inside the String
+        System.out.println(firstName.length());      // for accessing the length
+        System.out.println(firstName.charAt(1));    // Know the Character at particular index
+    }
+}
