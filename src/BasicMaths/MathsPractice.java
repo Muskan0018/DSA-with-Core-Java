00f8a3298ceea3ae6fd09fa30f16e60509d766ea
muskanmishra0607 <muskanmishra1806@gmail.com>
2026-08-01 18:14:33 +0530

    Basic Maths Completed

--- src/BasicMaths/MathsPractice.java
+++ src/BasicMaths/MathsPractice.java
@@ -109,6 +109,109 @@ public class MathsPractice {
     }
 
 // LCM of a Number
+    // formula- LCM * GCD(HCF) = a*b || LCM = (a*b)/GCD
+    static int getLCM(int a, int b) {
+        int gcd =  getGDC(a, b);
+        int prod = a * b;
+        int lcm = prod / gcd;
+        return lcm;
+    }
+
+// Armstrong Number
+    // for 3-digit value
+    // 153-> 1^3 + 5^3 + 3^3 = 153 (then it is armstrong number)
+//    static boolean isArmstrongNumber(int num) {
+//        int sum = 0;
+//        int originalNum = num;
+//        while(num != 0) {
+//            int digit = num % 10;
+//            int cubeOfDigit = digit * digit * digit;
+//            sum = sum + cubeOfDigit;
+//            num = num / 10;
+//        }
+//            if (sum == originalNum) {
+//                return true;
+//            }else {
+//                return false;
+//            }
+//    }
+
+    public static boolean isArmstrong(int num) {
+        int original = num;
+        int sum = 0;
+
+        // Count digits
+        int digits = String.valueOf(num).length();
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);  // we have a & b, pow(a, b) means a^b.
+            num /= 10;
+        }
+
+        return sum == original;
+    }
+
+// Find DIVISOR or CHECK PERFECT NUMBER
+//    static boolean checkPerfectNumber(int num) {
+//        int sum = 1;
+//
+//        for (int i=2; i<=Math.sqrt(num); i++) {
+//            if (num % i == 0) {
+//                // i ne num ko perfectly divide kr diya h
+//                // toh ab factor pair kya banega
+//                // 1st factor = i
+//                // 2nd factor = num / i
+//                int firstFactor = i;
+//                int secondFactor = num / i;
+//                sum = sum + firstFactor + secondFactor;
+//            }
+//        }
+//        if (sum == num) {
+//            return true;
+//        }else {
+//            return false;
+//        }
+//    }
+    // METHOD 2:
+    public static boolean isPerfect(int n) {
+
+        if (n <= 1) {
+            return false;
+        }
+
+        int sum = 0;
+
+        // Find sum of proper divisors
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == n;
+    }
+
+// Print all PRIME NUMBERS from 1 to N
+    static void printAllPrimeNumbers(int n) {
+        //print all prime numbers from 1 to n
+        // 1 is not a prime number
+        for (int num = 2; num <= n; num++) {
+            boolean isPrime = isPrimeOrNot(num);
+            if (isPrime == true) {
+                System.out.println(num);
+            }
+        }
+
+        // check whether a number is prime or not
+//        int num = 5;
+//        for (int i = 2; i*i < num; i++) {
+//            if (n % i == 0) {
+//                return false;
+//            }
+//            return true;
+//        }
+    }
 
     public static void main (String [] args) {
 
@@ -130,6 +233,16 @@ public class MathsPractice {
 //        System.out.println(isPrimeOrNot(num));
 
 //        System.out.println("GCD of a and b is: " + getGDC(18, 12));
+
+//        System.out.println("LCM of a and b is: " + getLCM(18, 12));
+
+//        System.out.println(isArmstrongNumber(153));
+//        System.out.println(isArmstrong(8208));
+
+//        System.out.println(checkPerfectNumber(6));
+//        System.out.println(isPerfect(6));
+
+        printAllPrimeNumbers(100);
 
     }
 }
