a250994a8b394a1da45bdd741cd5833ba0175d6e
muskanmishra0607 <muskanmishra0607@gmail.com>
2026-08-01 14:12:58 +0530

    updated

--- src/BasicMaths/MathsPractice.java
+++ src/BasicMaths/MathsPractice.java
@@ -55,9 +55,64 @@ public class MathsPractice {
             return revNum;
     }
 
+// Palindrome Number
+    static boolean isPalindrome(int num) {
+//        if (num < 0){ return false; }  // for negative values
+        int originalNum = num;
+        int reverseNum = reversedNumber(num);
+        if (originalNum == reverseNum) {
+            System.out.println(" It is a Palindrome.");
+            return true;
+        }
+        System.out.println(" It is not a Palindrome.");
+        return false;
+    }
+
+// Prime Number
+    static boolean isPrimeOrNot(int num) {
+        // this one is slower (it takes O(n) time complexity)
+//        for (int i = 2; i <= num - 1; i++) {
+//            if (num % i == 0) {
+//                // not a prime
+//                return false;
+//            }
+//        }
+
+        //faster one  (it takes O(sqrt n) time complexity)
+        for (int i = 2; i <= Math.sqrt(num); i++){    // or u can also write (i=2; i*i <= num; i++)
+            if (num % i == 0) {
+                // not a prime
+                return false;
+            }
+        }
+
+        // jab tak remainder zero nhi aata tab tak loop se bahar nhi aa paoge
+        // aur agar kabhi remainder 0 nahi aaya
+        // that means it is Prime
+
+        return true ;
+    }
+
+// Greatest Common Divisor (GCD) or Highest Common Factor (HCF) of a Number
+    // EUCLIDEAN ALGORITHM
+    static int getGDC(int a, int b) {
+        // gcd(a,b) = gcd(b, a%b)
+
+        while(b != 0) {   // b jab tak zero nhi ho rha tab tak operation perform krte rho
+            int oldValueOfB = b;
+            b = a % oldValueOfB;
+            a = oldValueOfB;
+        }
+        // jab b=0, tab a ki place pe GCD milega
+        int ans = a;
+        return ans;
+    }
+
+// LCM of a Number
+
     public static void main (String [] args) {
 
-        int num = 53127;
+//        int num = 123;
 
 //        printDigits(num);
 
@@ -67,7 +122,14 @@ public class MathsPractice {
 //        int sum = sumOfDigits(num);
 //        System.out.println("Sum: " + sum);
 
-        int rev = reversedNumber(num);
-        System.out.println("Reversed Number: " + rev);
+//        int rev = reversedNumber(num);
+//        System.out.println("Reversed Number: " + rev);
+
+//        boolean ans = isPalindrome(121);
+
+//        System.out.println(isPrimeOrNot(num));
+
+//        System.out.println("GCD of a and b is: " + getGDC(18, 12));
+
     }
 }
