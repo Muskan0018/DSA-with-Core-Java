d3ac619fad3a26977279c1fe1927b444059421a1
muskanmishra0607 <muskanmishra1806@gmail.com>
2026-08-03 12:03:43 +0530

    3 ques solved

--- src/BasicMaths/geeksOfgeeks.java
+++ src/BasicMaths/geeksOfgeeks.java
@@ -8,13 +8,11 @@ public class geeksOfgeeks {
             if (n <= 1) {
                 return false;
             }
-
             for (int i = 2; i * i <= n; i++) {
                 if (n % i == 0) {
                     return false;
                 }
             }
-
             return true;
         }
         public static int sumOfPrime(int n) {
@@ -28,9 +26,33 @@ public class geeksOfgeeks {
             }
             return sum;
         }
+
+//Given a number x, the task is to print the numbers from x to 0 in decreasing order in a single line.
+//Example:
+//Input: x = 3
+//Output: 3 2 1 0
+//Explanation: Numbers in decreasing order from 3 are 3 2 1 0.
+    // While Loop Question
+
+    public static void utility(int x) {
+        while(x>=0){
+            System.out.print("No. in decreasing order: " +x+" ");
+            x--;
+        }
+    }
+
+
+
+
+
+
         public static void main(String[] args) {
             int n = 10;
-            System.out.println(sumOfPrime(n));
+//            System.out.println(sumOfPrime(n));
+
+            int n1 = 5;
+            utility(n1);
         }
+
 }
 
---
+++ src/PracticeQuestions/BasicMathsQues.java
@@ -0,0 +1,18 @@
+package PracticeQuestions;
+
+public class BasicMathsQues {
+
+// Sum of Two Numbers
+    static int sumOfNumbers(int a, int b) {
+        return a + b;
+    }
+
+
+
+
+
+   public static void main(String[] args) {
+
+       System.out.println("Sum of two nums: " + sumOfNumbers(10, 8));
+    }
+}
