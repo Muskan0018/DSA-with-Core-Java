f4d45f3b92d4fbec54a5114ce2070e5aadc7c94f
muskanmishra0607 <muskanmishra1806@gmail.com>
2026-08-04 23:43:14 +0530

    OOPs started

---
+++ src/OOPsConcept/Student.java
@@ -0,0 +1,31 @@
+package OOPsConcept;
+
+// Class and Object (Creating an Object)
+
+public class Student {
+    // Attributes
+    public int id;
+    public int age;
+    public String name;
+    public int nos;
+
+    // Constructor
+    public Student() {
+        System.out.println("Student default constructor called");
+    }
+
+    // Methods or Behavior
+    public void study() {
+        System.out.println("Studying");
+    }
+    public void sleep() {
+        System.out.println("Sleeping");
+    }
+    public void bunk() {
+        System.out.println("Bunking");
+    }
+
+    static void main() {
+
+    }
+}
