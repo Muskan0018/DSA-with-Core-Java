638eb19b36e39f31f339add8ddea2c761b3fd0a3
muskanmishra0607 <muskanmishra1806@gmail.com>
2026-08-04 12:52:45 +0530

    2 problem solved

--- src/PracticeQuestions/BasicMathsQues.java
+++ src/PracticeQuestions/BasicMathsQues.java
@@ -7,12 +7,27 @@ public class BasicMathsQues {
         return a + b;
     }
 
+// Subtract Two Numbers
+    static int subtractNums(int a, int b) {
+        return a - b;
+    }
 
+// Multiply Two Numbers
+    static int multiplyNum(int a, int b) {
+        return a * b;
+    }
 
+// Divide Two Numbers
+    static int divideNums(int a, int b) {
+        return  a / b;
+    }
 
 
    public static void main(String[] args) {
 
-       System.out.println("Sum of two nums: " + sumOfNumbers(10, 8));
+//       System.out.println("Sum of two nums: " + sumOfNumbers(10, 8));
+//       System.out.println("Difference is: " + subtractNums(36, 18));
+//       System.out.println("Multiplication is: " + multiplyNum(2, 3));
+       System.out.println("Division is: " + divideNums(12, 2));
     }
 }
