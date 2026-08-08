ebf78842582963a0182dea915dd8f0cfb86e1f3d
muskanmishra0607 <muskanmishra1806@gmail.com>
2026-08-09 01:57:21 +0530

    updated

--- src/OOPsConcept/Polymorphism/RuntimePolymorphism.java
+++ src/OOPsConcept/Polymorphism/RuntimePolymorphism.java
@@ -18,12 +18,29 @@ class Circle extends Shape {
     }
 }
 
+class Rectangle extends Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Rectangle is drawing");
+    }
+}
+
 
 public class RuntimePolymorphism {
     public static void main(String[] args) {
 
         Circle c = new Circle();
-
         c.draw();
+
+        Rectangle r = new Rectangle();
+        r.draw();
+
+ // Dynamic Method Dispatch is the mechanism Java uses to decide at runtime which overridden method should be called.
+
+// Parent reference + child object → Java chooses the child's overridden method at runtime.
+        Shape s = new Rectangle();
+        s.draw();
+
     }
 }
