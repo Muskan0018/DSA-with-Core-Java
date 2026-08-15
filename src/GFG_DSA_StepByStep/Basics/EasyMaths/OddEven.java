14a4aebfdbacb955c231b425f00a22e14c24d332
muskanmishra0607 <muskanmishra1806@gmail.com>
2026-08-15 15:43:01 +0530

    updated

--- src/GFG_DSA_StepByStep/Basics/EasyMaths/OddEven.java
+++ src/GFG_DSA_StepByStep/Basics/EasyMaths/OddEven.java
@@ -1,4 +1,17 @@
 package GFG_DSA_StepByStep.Basics.EasyMaths;
 
+//Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false if the number is odd.
+
 public class OddEven {
+
+    static boolean isOddOrEven(int n) {
+        if (n % 2 == 0){
+            return true;
+        }
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isOddOrEven(18));
+    }
 }
