04e5f44842d2fb8a4ded2a2535624bbd29834273
muskanmishra0607 <muskanmishra1806@gmail.com>
2026-08-16 14:32:23 +0530

    ques 2 done

--- src/GFG_DSA_StepByStep/Basics/EasyRecursion/Factorial.java
+++ src/GFG_DSA_StepByStep/Basics/EasyRecursion/Factorial.java
@@ -1,16 +1,16 @@
-//package GFG_DSA_StepByStep.Basics.EasyRecursion;
-//
-//public class Factorial {
-//
-//    static int factorialOfNum(int n) {
-//        int fact = 1;
-//        for (int i = 2; i <= n; i++){
-//            fact *= i;
-//        }
-//        return fact;
-//    }
-//
-//    public static void main(String[] args) {
-//        System.out.print("Factorial of a given number is: " + factorialOfNum(5));
-//    }
-//}
+package GFG_DSA_StepByStep.Basics.EasyRecursion;
+
+public class Factorial {
+
+    static int factorialOfNum(int n) {
+        int fact = 1;
+        for (int i = 2; i <= n; i++){
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Factorial of a given number is: " + factorialOfNum(5));
+    }
+}
