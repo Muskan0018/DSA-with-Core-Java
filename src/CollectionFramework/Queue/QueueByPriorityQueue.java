4796ecdd743d5e645053575f9f50147336277493
muskanmishra0607 <muskanmishra1806@gmail.com>
2026-08-23 02:37:45 +0530

    updated

--- src/CollectionFramework/Queue/QueueByPriorityQueue.java
+++ src/CollectionFramework/Queue/QueueByPriorityQueue.java
@@ -1,5 +1,8 @@
 package CollectionFramework.Queue;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class QueueByPriorityQueue {
     public static void main(String[] args) {
 
@@ -17,6 +20,29 @@ public class QueueByPriorityQueue {
 
 
         // PriorityQueue implementation
+//        Queue<Integer> pq = new PriorityQueue<>();
+
+        // Min Heap → smallest element has the highest priority.
+
+        // if we want to give High Priority to the high value Integer.
+           // we can do this by using comparator
+        Queue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);   // using this Lambda Function (comparator) we can converted code into maxHeap
+         // Max Heap → largest element has the highest priority.
+          // maxHeap -> it says that in the case of Integers: the Higher the Integer Value, the Higher its Priority
+
+        pq.offer(40);
+        pq.offer(10);
+        pq.offer(20);
+        pq.offer(30);
+
+        System.out.println("Priority Queue: " + pq);
+
+        // default behavior for Integer -> less value = High Priority (through minHeap)
+        System.out.println("poll 1: " + pq.poll());      // removal order is: 10 -> 20 -> 30 -> 40
+        System.out.println("Queue after poll 1: " + pq);
+
+        System.out.println("poll 2: " + pq.poll());
+        System.out.println("Queue after poll 2: " + pq);
 
     }
 }
