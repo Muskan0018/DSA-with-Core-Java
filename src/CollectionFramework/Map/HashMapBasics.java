8547ddc80eebc885eff030aaddd3bf13123e8a68
muskanmishra0607 <muskanmishra1806@gmail.com>
2026-09-22 00:45:35 +0530

    implementing Map methods

--- src/CollectionFramework/Map/HashMapBasics.java
+++ src/CollectionFramework/Map/HashMapBasics.java
@@ -31,22 +31,46 @@ public class HashMapBasics {
         mapping.put("in", "India1");
         mapping.put("en", "England");
         mapping.put("us", "United States");
-        mapping.put("nz", "NewZeland");
+        mapping.put("nz", "NewZealand");
         mapping.put("in", "India");    // If key is already present, the new value replaces the old value
         System.out.println("Map1: " + mapping);
 
-        Map<String, String> county = new HashMap<>();
+        Map<String, String> country = new HashMap<>();
 
-        county.put("br", "Brazil");
-        System.out.println("Before: " + county);
+        country.put("br", "Brazil");
+        System.out.println("Before: " + country);
 
-    // putAll() -Inserts all the entries from Specific Map to this Map
+    // putAll() - Inserts all the entries from Specific Map to this Map
 
-        county.putAll(mapping);
-        System.out.println("Afer putAll(): " + county);
+        country.putAll(mapping);
+        System.out.println("Afer putAll(): " + country);
 
-    // putIf
+    // remove
+        country.remove("us");
+        System.out.println("Remove: " + country);
 
+    // putIfAbsent(K, V)- Insert the association if the key K is not already associated with the value V.
+        country.putIfAbsent("us", "United States");
+        System.out.println("putIfAbsent: " + country);
+
+        country.putIfAbsent("ind", "India");
+        System.out.println("updated: " + country);
+
+    /* get(K) - Returns the value associated with the specified key K.
+                If the key is not found, it returns null.
+     */
+        System.out.println("get(): " + country.get("br"));
+
+    /*  getorDefault(K, defaultValue) - Returns the value associated with the specified key K.
+                                        If the key is not found, it returns the defaultValue.
+     */
+        System.out.println("Defualt: " + country.getOrDefault("un", "United Nation"));
+
+    // containsKey(K) - Check it the specified key k is present in the map or not.
+        System.out.println("Key Contains: " + country.containsKey("in"));
+
+    // containsValue(V) -  Check it the specified value V is present in the map or not.
+        System.out.println("Value Contains: " + country.containsValue("NewZealand"));
     }
 }
 
