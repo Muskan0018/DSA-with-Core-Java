030b8c57f28b7252b7fbec3a9d610ec772dd822a
muskanmishra0607 <muskanmishra1806@gmail.com>
2026-09-23 01:52:08 +0530

    Comparable & Comparator started

---
+++ src/CollectionFramework/ComparableAndComparatorEx/Main.java
@@ -0,0 +1,20 @@
+package CollectionFramework.ComparableAndComparatorEx;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(18);
+        list.add(45);
+        list.add(17);
+        list.add(7);
+        System.out.println("list: " + list);
+
+//        Collections.sort(list);
+//        System.out.println(list);
+    }
+}
