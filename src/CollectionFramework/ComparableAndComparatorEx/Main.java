5a13df51a9fc63649c5358ed98cf95730344aabe
muskanmishra0607 <muskanmishra1806@gmail.com>
2026-09-24 00:06:28 +0530

    Comparable

--- src/CollectionFramework/ComparableAndComparatorEx/Main.java
+++ src/CollectionFramework/ComparableAndComparatorEx/Main.java
@@ -7,14 +7,28 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>();
-        list.add(18);
-        list.add(45);
-        list.add(17);
-        list.add(7);
-        System.out.println("list: " + list);
+        List<Students> students = new ArrayList<>();
 
-//        Collections.sort(list);
-//        System.out.println(list);
+        students.add(new Students(19, "Muskan", 48));
+        students.add(new Students(21, "Pratiksha", 50));
+        students.add(new Students(24, "Richa", 65));
+        students.add(new Students(25, "Rishank", 75));
+
+        System.out.println("Students List: " + students);
+
+
+
+
+
+
+//        List<Integer> list = new ArrayList<>();
+//        list.add(18);
+//        list.add(45);
+//        list.add(17);
+//        list.add(7);
+//        System.out.println("list: " + list);
+//
+////        Collections.sort(list);
+////        System.out.println(list);
     }
 }
---
+++ src/CollectionFramework/ComparableAndComparatorEx/Students.java
@@ -0,0 +1,52 @@
+package CollectionFramework.ComparableAndComparatorEx;
+
+public class Students implements Comparable {
+    public int age;
+    public String name;
+    public int weight;
+
+    public Students(int age, String name, int weight) {
+        this.age = age;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age: " + age +
+                ", name: '" + name + '\'' +
+                ", weight: " + weight +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Object that) {
+        // this method is called for current object
+        // we will define our sorting logic
+    }
+}
